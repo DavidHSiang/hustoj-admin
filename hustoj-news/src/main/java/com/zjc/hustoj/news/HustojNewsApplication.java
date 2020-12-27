7c59e581e8f8edeb9209a2155185f80b181dbaeb
david <ls5129469@163.com>
2020-12-27 19:44:22 +0800

    add hustoj-news

---
+++ hustoj-news/src/main/java/com/zjc/hustoj/news/HustojNewsApplication.java
@@ -0,0 +1,13 @@
+package com.zjc.hustoj.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HustojNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HustojNewsApplication.class, args);
+    }
+
+}
