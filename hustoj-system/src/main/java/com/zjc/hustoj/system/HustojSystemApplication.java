8e81c299aae0bbdb8dbd1bde2dc7a6be5bc5b25d
david <ls5129469@163.com>
2020-12-27 19:41:15 +0800

    add hustoj-system

---
+++ hustoj-system/src/main/java/com/zjc/hustoj/system/HustojSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zjc.hustoj.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HustojSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HustojSystemApplication.class, args);
+    }
+
+}
---
+++ hustoj-system/src/test/java/com/zjc/hustoj/system/HustojSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjc.hustoj.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HustojSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
