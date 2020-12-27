6655bcfe5c8c132478dd19ae6dcb5689a2810bcb
david <ls5129469@163.com>
2020-12-27 20:20:38 +0800

    add hustoj-admin

---
+++ hustoj-admin/src/main/java/com/zjc/hustoj/admin/HustojAdminApplication.java
@@ -0,0 +1,13 @@
+package com.zjc.hustoj.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HustojAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HustojAdminApplication.class, args);
+    }
+
+}
---
+++ hustoj-admin/src/test/java/com/zjc/hustoj/admin/HustojAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjc.hustoj.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HustojAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
