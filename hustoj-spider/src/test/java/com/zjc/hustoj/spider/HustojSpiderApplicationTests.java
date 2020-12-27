1a49873c01d38b6aa3975bb800bda60f1249f1b7
david <ls5129469@163.com>
2020-12-27 19:37:42 +0800

    add hustoj-spider

---
+++ hustoj-spider/src/test/java/com/zjc/hustoj/spider/HustojSpiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjc.hustoj.spider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HustojSpiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
