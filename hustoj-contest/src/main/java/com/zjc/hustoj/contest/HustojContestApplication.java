e629689d17b7c41a74a1bf3392db0880a730d920
david <ls5129469@163.com>
2020-12-27 20:14:02 +0800

    add hustoj-contest

---
+++ hustoj-contest/src/main/java/com/zjc/hustoj/contest/HustojContestApplication.java
@@ -0,0 +1,13 @@
+package com.zjc.hustoj.contest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HustojContestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HustojContestApplication.class, args);
+    }
+
+}
---
+++ hustoj-contest/src/test/java/com/zjc/hustoj/contest/HustojContestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjc.hustoj.contest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HustojContestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
